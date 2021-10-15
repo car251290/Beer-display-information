91693c59c037e0fe5f4729f0641b0caf1608f407
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-10-14 20:40:51 -0500

    Add files via upload

--- src/componets/beers.js
+++ src/componets/beers.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
-import '../styles/style.css'
+//import '../styles/style.css'
+import { Typography,Card, CardContent,Box,CardMedia} from '@material-ui/core';
+import useStyles from './style';
+
 class Beers extends Component {
-
-  render() {
+ 
+  render () {
+   //const classes = useStyles();
     const items = this.props.beers.map((beer) => {
+      
       return (
-        <li key={beer.id}>
-          <h3>{beer.name}</h3>
-          <h4>{beer.images}</h4>
-
-          <p>{beer.description}</p>
-        </li>
+        <Card elevation={6}
+        title={beer.name}
+        display="flex"
+        >
+           <CardMedia
+        style={{ height: 350 }}
+        image={beer.image_url}
+        title={beer.name}
+      />
+          <CardContent style={{backgroundColor:"#F28304"}}>
+            <Typography gutterBotton variant="h5">{beer.name}</Typography>
+            <Box display="flex-wrap" justifyContent="space-between" my={2} alignItems="center">
+            </Box>
+          <Box display="flex" justifyContent="space-between">
+          <Typography component="legend">city</Typography>
+          <Typography gutterBottom variant="subtitle1">
+            {beer.city}
+          </Typography>
+        </Box>
+          </CardContent>
+        </Card>
       )
     });
 
@@ -19,7 +39,8 @@ class Beers extends Component {
         {items}
       </ul>
     );
-  }
+}
+  
 }
 
 export default Beers;

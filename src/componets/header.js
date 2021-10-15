fa829d6707500aa584c247ec7d3d5de3212ad884
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-10-14 20:41:22 -0500

    Add files via upload

--- src/componets/header.js
+++ src/componets/header.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import '../styles/style.css'
-
-
-class Header extends Component {
-
-  render() {
+import {AppBar,Toolbar,Typography,InputBase,Box} from '@material-ui/core';
+import useStyles from './style';
+const Header = () => {
+  const classes = useStyles();
     return (
-      <header>
-        <h1>Beer information</h1>
-      </header>
+      <AppBar position ="static">
+        <Toolbar className={classes.toolbar}>
+        <Typography variant= "h5" className={classes.title}>
+           Beer Information
+        </Typography>
+        </Toolbar>
+      </AppBar>
+     
+      
     );
-  }
+  
 }
 
 export default Header;

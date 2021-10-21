c5230eec46a5859b1433d53f61e66f538226ce52
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-10-21 18:12:30 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import App from './App';
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDom.render(<App/>, document.getElementById('root'));
 
 

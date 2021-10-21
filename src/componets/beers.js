fb542e764b315f9a9f620a9d1fa15703cb8cfeea
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-10-21 18:11:31 -0500

    Add files via upload

--- src/componets/beers.js
+++ src/componets/beers.js
@@ -1,40 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
+//import '../styles/style.css'
 import { Typography,Card, CardContent,Box,CardMedia} from '@material-ui/core';
 import useStyles from './style';
+import index from '../Api/index'
 
-class Beers extends Component {
- 
-  render () {
+const Beers = () => {
+const [beers, setbeers] = useState({})
 
-    const items = this.props.beers.map((beer) => {
-      
+useEffect(()=> {
+  setbeers( () => (beers));
+},[beers])
       return (
         <Card elevation={6}
-        title={beer.name}
+        title={beers.name}
         display="flex"
         >
         <CardContent style={{backgroundColor:"#F28304"}}>
-            <Typography gutterBotton variant="h5">{beer.name}</Typography>
+            <Typography gutterBotton variant="h5">{beers.name}</Typography>
             <Box display="flex-wrap" justifyContent="space-between" my={2} alignItems="center">
             </Box>
           <Box display="flex" justifyContent="space-between">
           <Typography component="legend">description</Typography>
           <Typography gutterBottom variant="subtitle1">
-            {beer.description}
+            {beers.description}
           </Typography>
         </Box>
           </CardContent>=
         </Card>
       )
-    });
-
-    return (
-      <ul>
-        {items}
-      </ul>
-    );
-}
-  
 }
 
 export default Beers;

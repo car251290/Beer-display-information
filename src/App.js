a1b106bac83f482681e81c54de50e264ff0f3acc
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-10-22 16:57:54 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {useEffect, useState } from 'react';
-import { render } from 'react-dom';
+import { Grid,Card, CardContent,Typography,Box} from '@material-ui/core';
 import Header from './componets/Header/header';
 import Beers from './componets/Beer/beers';
 import './styles/style.css';
 import './App.css'
 import {  getBeerData } from './componets/Api/index';
-
+import useStyles from './styles/style';
 const App = () => {
+  const classes = useStyles();
   const [beers, setbeers] = useState([])
  
  useEffect (() => {
@@ -18,18 +19,32 @@ const App = () => {
      }
    })
    return () => beers = false;
-   //.then((data)=> {setbeers([])})
-
+   
  },[]);
   
     return (
     <>
-     
      <Header />
-        <Beers beers={setbeers} />
-        
+
+        <Card elevation={6} display="flex">
         {beers.map(items => items.name)}
-        
+       
+        <CardContent style= {{backgroundColor:"#F28304"}}>
+        <Typography gutterBotton variant="h5">{beers.map(items => items.name)} </Typography>
+        <Box display="flex-wrap" justifyContent="space-between" my={2} alignItems="center">
+        </Box>
+
+        <Box display="flex" justifyContent="space-between">
+          <Typography component="legend">description</Typography>
+          <Typography gutterBottom variant="subtitle1">
+            {beers.map(items => (items.description))}
+          </Typography>
+        </Box>
+
+        </CardContent>
+
+        </Card>
+  
     </>
     );
   

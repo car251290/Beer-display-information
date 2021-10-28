c41021c9a838a08e7a06dbf6eac30eb8d674a812
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-10-27 21:06:05 -0500

    Add files via upload

--- src/componets/beers.js
+++ src/componets/beers.js
@@ -1,29 +1,29 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component, createRef, useEffect, useState } from 'react';
 //import '../styles/style.css'
 import { Typography,Card, CardContent,Box,CardMedia} from '@material-ui/core';
 import useStyles from './style';
 import index from '../Api/index'
 
-const Beers = ({beers}) => {
+const Beers = ({items}) => {
 const [thebeers, setthebeers] = useState([])
 
-useEffect(()=> {
-  setthebeers( (thebeers) => (beers));
-},[beers])
+useEffect( () => {
+  setthebeers( (refs) => Array(items.length).map((_,i) => refs[i] || createRef()));
+},[items])
       return (
         <Card elevation={6}
-        title={beers.name}
+        title={items.name}
         display="flex"
         >
         <CardContent style={{backgroundColor:"#F28304"}}>
-            <Typography gutterBotton variant="h5">{beers.name}</Typography>
+            <Typography gutterBotton variant="h5">{items.name}</Typography>
             <Box display="flex-wrap" justifyContent="space-between" my={2} alignItems="center">
             </Box>
 
           <Box display="flex" justifyContent="space-between">
-          <Typography component="legend">description</Typography>
+          <Typography component="legend">Beer description</Typography>
           <Typography gutterBottom variant="subtitle1">
-            {beers.description}
+            {items.description}
           </Typography>
         </Box>
         

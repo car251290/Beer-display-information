b43aa025460eab7f9c7dba3212a3757e682fa179
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-10-28 18:20:47 -0500

    Add files via upload

--- src/componets/index.js
+++ src/componets/index.js
@@ -1,26 +1,12 @@
 import axios from "axios";
 
 export const getBeerData = async () => {
+  try {
     return fetch('https://api.punkapi.com/v2/beers')
-      .then(data => data.json())   
+      .then(data => data.json())  
+  } catch (error) {
+    return error;
+
+  }
+    
 }
-
- //' https://api.openbrewerydb.org/breweries'
-  //constructor() {
-    //super();
-
-   // this.state = {
-    //  beers: []
-   // }
- // }
-
- // componentDidMount() {
-   // fetch('https://api.punkapi.com/v2/beers').then((response) => {
-     // return response.json();
-    //}).then((data) => {
-      //console.log(data);
-      //this.setState({
-        //beers: data
-      //})
-    //})
- // }
